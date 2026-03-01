211b0de748c5dd630757fd18abf457f9456ba573
AG1713 <avdhootgole@gmail.com>
2026-03-01 12:59:18 +0530

    fixed comments typos

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,8 @@ app = FastAPI()
 def attack_summary():
     return generate_attack_summary()
 
-# Note that before running the below you have created a virtual environment.
-# Also, u should do 'venv\Scripts\activate' before installing the requirements.txt
-
 # Before running backend:
-#   1) Make sure to create a venv, if not, command:
+#   1) Make sure to create a venv, if not already, command:
 #       python -m venv venv
 #   2) Make sure to activate the venv, if not already, command:
 #       venv\Scripts\activate
