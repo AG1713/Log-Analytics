fbab8d91629bda314c8276ff953443cae63defbb
nirajbagde2004 <nirajbagde2004@gmail.com>
2026-03-20 22:54:28 +0530

    Changes in main.py and agent.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://172.17.0.1:5173","http://172.19.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "http://172.17.0.4:5173",
+        "http://172.17.0.1:5173"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
