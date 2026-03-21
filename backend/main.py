4f3b3da64b1381b22ec9a06014ac75fdd51cdd0d
AG1713 <avdhootgole@gmail.com>
2026-03-21 18:46:18 +0530

    Dashboard and Sidebar minor changes

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173", 
+        "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://172.17.0.4:5173",
         "http://172.17.0.1:5173"
